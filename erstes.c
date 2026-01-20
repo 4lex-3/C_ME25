9ece5ee3124abc40dae5c8e9ee2aa14bf6ea1b2e
Alex <github.hedging156@passmail.com>
2026-01-20 11:26:01 +0100

    Add files via upload

---
+++ erstes.c
@@ -0,0 +1,20 @@
+//
+// Created by alexm on 11.11.2025.
+// Version 1.0 erstes programm ME25 Stempfle
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main() {
+
+    char x [] = "\0";
+
+    printf("\"Guten Morgen\" ihr C-Schwuchtel\n");
+    fgets(x, sizeof(x), stdin);
+    x[strlen(x) - 1] = '\0';
+    printf("%s\n", x);
+
+
+    return (EXIT_SUCCESS);
+}
